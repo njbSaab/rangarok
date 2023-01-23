3414858c93ef385b35fe90c8e0addd168e13a6e6
Pro <Admin@Pro.local>
2023-01-23 21:28:43 +0200

    F

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,193 +1,203 @@
 import "swiper/swiper.min.css";
 import "../styles/reset.scss";
+import "../styles/mixins.scss";
 import "../styles/styles.scss";
+// import { version } from "html-webpack-plugin";
 
-import Swiper, { Navigation } from "swiper";
-import { languages } from "./languages";
-Swiper.use([Navigation]);
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination])
 
-const checkboxes = {
-  requirements: ["minimum", "recommended"],
-  versions: ["standard", "limited"],
-};
-let isPlay = false;
 const classes = {
   opened: "opened",
   hidden: "hidden",
-  active: "active",
-};
-const values = [
-  {
-    price: 19.99,
-    title: "Standard Edition",
-  },
-  {
-    price: 18.99,
-    title: "Standard Edition",
-  },
-  {
-    price: 29.99,
-    title: "Deluxe Edition",
-  },
-];
+  active: "active"
 
-const checkbox = document.querySelectorAll(".checkbox");
+};
+
+//массив из списка характеристик в карточке игрока
+const checkboxes = {
+   requirements: ["minimum", 'recommended'],
+   version: ["standart", 'limited']
+};
+
+const values = [
+   {  
+      title: "Standart Edition",
+      price: 19.99,
+   },
+   {
+      title: "Standart Edition",
+      price: 18.99,
+   },
+   {
+      title: "Delux Edition",
+      price: 29.99,
+   },
+]
+
+const checkBox = document.querySelectorAll(".checkbox");
 const header = document.querySelector(".header");
+const menuBtn = document.querySelector(".header-menu__button");
 const menuLink = document.querySelectorAll(".menu-link");
-const menuButton = document.querySelector(".header-menu__button");
 const video = document.getElementById("video");
-const videoButton = document.querySelector(".video-btn");
+const videoBtn = document.querySelector(".video-btn");
 const faqItem = document.querySelectorAll(".faq-item");
-const sections = document.querySelectorAll(".section");
-const language = document.querySelectorAll(".language");
+const section = document.querySelectorAll(".section");
 const buyButton = document.querySelectorAll(".buy-button");
 const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const modalTitle = document.querySelector(".modal-version");
 const modalPrice = document.querySelector(".modal-total__price");
+const modalTitle = document.querySelector(".modal-title");
 const modalClose = document.querySelector(".modal-close");
+const overlay = document.querySelector(".overlay");
 
-const toggleMenu = () => header.classList.toggle(classes.opened);
-const scrollToSection = (e) => {
-  e.preventDefault();
-  const href = e.currentTarget.getAttribute("href");
+let isPlay = false;
 
-  if (!href && !href.startsWith("#")) return;
+//? burger
+const toggleMenu = () => header.classList.toggle(classes.opened); // добавление класс opened при клике на header-menu__button
+
+//? video
+const handleVideo = ({target}) => {
+   const info = target.parentElement; 
+
+   isPlay = !isPlay;
+   info.classList.toggle(classes.hidden, isPlay)
+   target.innerText = isPlay ? 'Pause' : 'Play';
+   isPlay ? video.play() : video.pause()
+}
+
+//плавный скрол на якорям по страници //? scrollToSection
+const scrollToSection = (elem) => {
+  elem.preventDefault();
+  const href = elem.currentTarget.getAttribute("href");
+
+  if (!href && !href.startWith("#")) return;
 
   const section = href.slice(1);
   const top = document.getElementById(section)?.offsetTop || 0;
   window.scrollTo({ top, behavior: "smooth" });
 };
 
-const formatValue = (value) => (value < 10 ? `0${value}` : value);
-
-const getTimerValues = (diff) => ({
-  seconds: (diff / 1000) % 60,
-  minutes: (diff / (1000 * 60)) % 60,
-  hours: (diff / (1000 * 3600)) % 24,
-  days: (diff / (1000 * 3600 * 24)) % 30,
-});
-
-const setTimerValues = (values) => {
-  Object.entries(values).forEach(([key, value]) => {
-    const timerValue = document.getElementById(key);
-    timerValue.innerText = formatValue(Math.floor(value));
+//? таймер
+//конвертируем время в миллисекундах в необходимые показатели
+const getTimerVals = (diff) => 
+ ({
+    seconds: (diff / 1000) % 60,
+    minutes: (diff / (1000 * 60)) % 60,
+    hours: (diff / (1000 * 3600)) % 24,
+    days: (diff / (1000 * 3600 * 24)) % 30,
+    mounth: (diff / (1000 * 3600 * 24 * 30)) % 12,
   });
-};
 
-const startTimer = (date) => {
-  const id = setInterval(() => {
-    const diff = new Date(date).getTime() - new Date().getTime();
+const formatVal =(value)=> value < 10? `0${value}` : value;
 
-    if (diff < 0) {
+const startTimer =(date)=> {
+  setInterval(() => {
+   const diff = new Date(date).getTime() - new Date().getTime(); //вычисляем текущею дату от заданой 
+   const vals = getTimerVals(diff) // кладем в переменную разницу в миллисеках
+   
+   //очистка таймера 
+   if (diff < 0) {
       clearInterval(id);
       return;
-    }
+   }
+   //получаем обьект перебераем его
+   Object.entries(vals).forEach(([key, value])=> {
+     const timerVal = document.getElementById(key) //доступ по ключам к айдишникам 
+     timerVal.innerText = formatVal(Math.floor(value)) //вывод данных
+   })
+  }, 1000);//каждую секунду выводим 
 
-    setTimerValues(getTimerValues(diff));
-  }, 1000);
 };
+//? таймер
 
-const handleVideo = ({ target }) => {
-  const info = target.parentElement;
+//? переключатель по  data-requirements + id
+const handleCheck = ({currentTarget: {checked, name}}) => {
+   const {active} = classes;
+   const value = checkboxes[name][Number(checked)];
+   const list = document.getElementById(value);
+   const tabs = document.querySelectorAll(`[data-${name}]`);
+   const siblings = list.parentElement.children;
 
-  isPlay = !isPlay;
-  info.classList.toggle(classes.hidden, isPlay);
-  target.innerText = isPlay ? "Pause" : "Play";
-  isPlay ? video.play() : video.pause();
-};
+   //для списка
+   for(const item of siblings) item.classList.remove(active);
+   //для тайтла
+   for(const tab of tabs) {;
+      tab.classList.remove(active)
+      tab.dataset[name] === value && tab.classList.add(active);
+   }
 
-const handleCheckbox = ({ currentTarget: { checked, name } }) => {
-  const { active } = classes;
-  const value = checkboxes[name][Number(checked)];
-  const list = document.getElementById(value);
-  const tabs = document.querySelectorAll(`[data-${name}]`);
-  const siblings = list.parentElement.children;
 
-  for (const item of siblings) item.classList.remove(active);
-  for (const tab of tabs) {
-    tab.classList.remove(active);
-    tab.dataset[name] === value && tab.classList.add(active);
-  }
+   list.classList.add(active)
+   // console.log(value);
+}
 
-  list.classList.add(active);
-};
-
+//? slider
 const initSlider = () => {
-  new Swiper(".swiper", {
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 20,
-    initialSlide: 2,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-};
-
-const handleFaqItem = ({ currentTarget: target }) => {
-  target.classList.toggle(classes.opened);
-  const isOpened = target.classList.contains(classes.opened);
-  const height = target.querySelector("p").clientHeight;
-  const content = target.querySelector(".faq-item__content");
-
-  content.style.height = `${isOpened ? height : 0}px`;
-};
-
-const handleScroll = () => {
-  const { scrollY: y, innerHeight: h } = window;
-  sections.forEach((sec) => {
-    if (y > sec.offsetTop - h / 1.5) sec.classList.remove(classes.hidden);
-  });
-};
-
-const setTexts = () => {
-  const lang = localStorage.getItem("lang") || "en";
-  const content = languages[lang];
-
-  Object.entries(content).forEach(([key, value]) => {
-    const items = document.querySelectorAll(`[data-text="${key}"]`);
-    items.forEach((item) => (item.innerText = value));
-  });
-};
-
-const toggleLanguage = ({ target }) => {
-  const { lang } = target.dataset;
-
-  if (!lang) return;
-
-  localStorage.setItem("lang", lang);
-  setTexts();
-};
-
-const handleBuyButton = ({ currentTarget: target }) => {
-  const { value } = target.dataset;
-
-  if (!value) return;
-
-  const { price, title } = values[value];
-
-  modalTitle.innerText = title;
-  modalPrice.innerText = `${price}$`;
-  modal.classList.add(classes.opened);
-  overlay.classList.add(classes.opened);
-};
-
-const closeModal = () => {
-  modal.classList.remove(classes.opened);
-  overlay.classList.remove(classes.opened);
-};
-
-setTexts();
+   new Swiper(".swiper", {
+      loop: true,
+      slidesPerView: 3,
+      spaceBetween: 20,
+      initialSlide: 2,
+      navigation: {
+         nextEl: ".swiper-button-next",
+         prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+         el: ".swiper-pagination",
+         dynamicBullets: true,
+       },
+   })
+}
 initSlider();
-startTimer("November 11, 2022 00:00:00");
-window.addEventListener("scroll", handleScroll);
-modalClose.addEventListener("click", closeModal);
-menuButton.addEventListener("click", toggleMenu);
-videoButton.addEventListener("click", handleVideo);
-menuLink.forEach((link) => link.addEventListener("click", scrollToSection));
-checkbox.forEach((box) => box.addEventListener("click", handleCheckbox));
-faqItem.forEach((item) => item.addEventListener("click", handleFaqItem));
-language.forEach((lang) => lang.addEventListener("click", toggleLanguage));
-buyButton.forEach((btn) => btn.addEventListener("click", handleBuyButton));
+
+//? faq
+const handleFaqItem = ({ currentTarget: target }) => {
+   target.classList.toggle(classes.opened);
+
+   const isOpened = target.classList.contains(classes.opened);
+   const height = target.querySelector("p").clientHeight;
+   const content = target.querySelector(".faq-item__content")
+
+   content.style.height = `${isOpened ? height : 0}px`
+}
+
+//! анимация класса hidden
+const heandleScroll =()=> {
+   const { scrollY: y, innerHeight: h} = window; // y - число скрола экрана
+   // console.log(y);
+   section.forEach((sectns) =>{
+      if(y>sectns.offsetTop - h / 1.5) sectns.classList.remove(classes.hidden)
+   })
+}
+
+//! модальное окно по клику кнопка купить
+const handleBuyBtn = ({currentTarget: target}) =>{
+   const {value} = target.dataset
+
+   if(!value) return
+
+   const {price, title} = values[value]
+
+   modalTitle.innerText = title;
+   modalPrice.innerText = `${price}$`;
+   modal.classList.add(classes.opened);
+   overlay.classList.add(classes.opened);
+}
+const closedModal = () => {
+   modal.classList.remove(classes.opened);
+   overlay.classList.remove(classes.opened);
+}
+
+
+startTimer("April 12, 2023 00:00:00");// заданная дата
+
+window.addEventListener('scroll', heandleScroll)//! по скролу анимация класса hidden
+videoBtn.addEventListener("click", handleVideo);  //? Video
+menuBtn.addEventListener("click", toggleMenu); //событие клик и добавление через функцию //? toggleMenu класса opened
+modalClose.addEventListener("click", closedModal); //? закрытие модального окна
+
+
+menuLink.forEach((link) => link.addEventListener("click", scrollToSection)); //по клику на меню запускамы функцию //? scrollToSection
+checkBox.forEach((box) => box.addEventListener("click", handleCheck)); //по клику на меню запускамы функцию //? scrollToSection
+faqItem.forEach((item) => item.addEventListener("click", handleFaqItem));// по клику выезжает item в faq section
+buyButton.forEach((btn) => btn.addEventListener("click", handleBuyBtn));// по клику выезжает //? модальное окно
